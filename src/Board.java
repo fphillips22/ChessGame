a3eafcdba6e8175c33292e65d285da0689563297
fphillips22 <fphillips1209@gmail.com>
2023-05-11 12:16:04 -0600

    fixed board for dark pieces

--- src/Board.java
+++ src/Board.java
@@ -29,22 +29,22 @@ public class Board {
         boxes[1][7] = new Spot(1,7 , new Pawn(true));
 
         // dark pieces
-        boxes[7][0] = new Spot(0,0, new Rook(true));
-        boxes[7][1] = new Spot(0,1, new Knight(true));
-        boxes[7][2] = new Spot(0,2, new Bishop(true));
-        boxes[7][3] = new Spot(0,3, new Queen(true));
-        boxes[7][4] = new Spot(0,4, new King(true));
-        boxes[7][5] = new Spot(0,5, new Bishop(true));
-        boxes[7][6] = new Spot(0,6, new Knight(true));
-        boxes[7][7] = new Spot(0,7, new Rook(true));
-        boxes[6][0] = new Spot(1,0 , new Pawn(true));
-        boxes[6][1] = new Spot(1,1 , new Pawn(true));
-        boxes[6][2] = new Spot(1,2 , new Pawn(true));
-        boxes[6][3] = new Spot(1,3 , new Pawn(true));
-        boxes[6][4] = new Spot(1,4 , new Pawn(true));
-        boxes[6][5] = new Spot(1,5 , new Pawn(true));
-        boxes[6][6] = new Spot(1,6 , new Pawn(true));
-        boxes[6][7] = new Spot(1,7 , new Pawn(true));
+        boxes[7][0] = new Spot(7,0, new Rook(true));
+        boxes[7][1] = new Spot(7,1, new Knight(true));
+        boxes[7][2] = new Spot(7,2, new Bishop(true));
+        boxes[7][3] = new Spot(7,3, new Queen(true));
+        boxes[7][4] = new Spot(7,4, new King(true));
+        boxes[7][5] = new Spot(7,5, new Bishop(true));
+        boxes[7][6] = new Spot(7,6, new Knight(true));
+        boxes[7][7] = new Spot(7,7, new Rook(true));
+        boxes[6][0] = new Spot(6,0 , new Pawn(true));
+        boxes[6][1] = new Spot(6,1 , new Pawn(true));
+        boxes[6][2] = new Spot(6,2 , new Pawn(true));
+        boxes[6][3] = new Spot(6,3 , new Pawn(true));
+        boxes[6][4] = new Spot(6,4 , new Pawn(true));
+        boxes[6][5] = new Spot(6,5 , new Pawn(true));
+        boxes[6][6] = new Spot(6,6 , new Pawn(true));
+        boxes[6][7] = new Spot(6,7 , new Pawn(true));
 
 
         for(int i = 2; i < 6; i++){
